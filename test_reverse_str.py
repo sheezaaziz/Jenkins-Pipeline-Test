8ef8688d919c591a3d0737734575938452cb6df8
Sheeza Aziz <sheeza.aziz@utoronto.ca>
2021-03-24 11:54:15 -0400

    added test file

---
+++ test_reverse_str.py
@@ -0,0 +1,10 @@
+def reverse(str):
+    reversed = ''
+    for i in range(len(str) -1, -1, -1):
+        reversed += str[i];
+    return reversed;
+
+def test_reverse():
+    greeting = 'Hello';
+    reverse_greeting = reverse(greeting);
+    assert reverse_greeting == 'olleH';
